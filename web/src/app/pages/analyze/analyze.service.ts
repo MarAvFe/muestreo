cd487c995209926876f604befcd84079ea146c0f
Marcello Avila <marcelloavila@gmail.com>
2017-09-06 17:19:08 -0600

    Se elimina referencia a archivo de pruebas borrado.

--- web/src/app/pages/analyze/analyze.service.ts
+++ web/src/app/pages/analyze/analyze.service.ts
@@ -4,7 +4,6 @@ import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { BaThemeConfigProvider } from '../../theme';
-import { TITLES } from './mock-titles';
 
 import { Titles } from './Titles';
 
