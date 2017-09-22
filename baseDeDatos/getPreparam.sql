296bcf43df7b562766937dbdffa8128986c628b0
kimcocru <kimcocru@gmail.com>
2017-09-22 14:22:30 -0600

    borrado archivo innecesario

--- baseDeDatos/getPreparam.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getPreparam`(pId_Sampling int, pDescription VARCHAR(255),pIdSamplingType int)
-BEGIN
- SELECT p_preliminar, q_preliminar, n_preliminar
- from Sampling
- WHERE idSampling = pId_Sampling and description = pDescription and SamplingType_idSamplingType = pIdSamplingType ;
- END
