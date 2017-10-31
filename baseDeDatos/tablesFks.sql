10500000a7261e3827f3597c269f58a6b21a0299
Marcello <marcelloavila@gmail.com>
2017-10-30 19:26:25 -0600

    Bugs.
    Error handling and logging. Muestreo preliminar y acciones.

--- baseDeDatos/tablesFks.sql
+++ baseDeDatos/tablesFks.sql
@@ -582,14 +582,14 @@ SHOW WARNINGS;
 -- -----------------------------------------------------
 
 USE `sampling`;
-DROP procedure IF EXISTS `sampling`.`getPreparam`;
+DROP procedure IF EXISTS `sampling`.`getPreParam`;
 SHOW WARNINGS;
 
 DELIMITER $$
 USE `sampling`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getPreparam`(pId_Sampling int, pDescription VARCHAR(1000),pIdSamplingType int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getPreParam`(pId_Sampling int, pDescription VARCHAR(1000),pIdSamplingType int)
 BEGIN
- SELECT p_preliminar, q_preliminar, n_preliminar
+ SELECT error_preliminar, n_preliminar
  from Sampling
  WHERE idSampling = pId_Sampling and description = pDescription and SamplingType_idSamplingType = pIdSamplingType ;
  END$$
@@ -844,16 +844,12 @@ DELIMITER $$
 USE `sampling`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `pUpPreParamsSampling`(pId_Sampling int, pDescription varchar(255),
 	pIdSamplingType int,
-	pp_preliminar double,
-	pq_preliminar double,
 	perror_preliminar double,
-	pn_preliminar int,
-	pz_preliminar double)
+	pn_preliminar int)
 BEGIN
 	UPDATE Sampling
     SET
-		p_preliminar = pp_preliminar,
-		q_preliminar = pq_preliminar,
+		error_preliminar = perror_preliminar,
 		n_preliminar = pn_preliminar
 	WHERE idSampling = pId_Sampling AND description = pDescription AND SamplingType_idSamplingType =  pIdSamplingType;
 END$$
